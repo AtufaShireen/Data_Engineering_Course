1dee27686a47c7a2dca71f620615a338774f41e1
Atufa Shireen <atufashireen@Atufas-MacBook-Air.local>
2022-06-04 00:42:06 +0530

    day 2-docker+postres

---
+++ week1/postgres.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+df = pd.read_parquet('/Users/atufashireen/projects/DE_ZoomCamp/datasets/yellow_tripdata_2021-01.parquet')
+
+df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+# print(pd.io.sql.get_schema(df,name='yello_dataset'))
+
+
+# from sqlalchemy import create_engine
+engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+df.head(n=10000).to_sql(name='yellow_taxt_data', con=engine, if_exists='replace')
+
