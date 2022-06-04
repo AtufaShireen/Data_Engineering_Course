8eb074fe5ff59651770b6d67439229106769874d
Atufa Shireen <atufashireen@Atufas-MacBook-Air.local>
2022-06-05 00:21:53 +0530

    day6 sql

--- week1/postgres.py
+++ week1/postgres.py
@@ -3,15 +3,15 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-df = pd.read_parquet('/Users/atufashireen/projects/DE_ZoomCamp/datasets/yellow_tripdata_2021-01.parquet')
+df = pd.read_csv('/Users/atufashireen/projects/DE_ZoomCamp/datasets/taxi-zone-lookup.csv')
 
-df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+# df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
+# df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 # print(pd.io.sql.get_schema(df,name='yello_dataset'))
 
 
 # from sqlalchemy import create_engine
 engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
 
-df.head(n=10000).to_sql(name='yellow_taxt_data', con=engine, if_exists='replace')
+df.to_sql(name='zones', con=engine, if_exists='replace')
 
